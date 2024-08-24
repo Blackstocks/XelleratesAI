b698bddfdf251d89d6a18c3d0ce57ec45af6fea4
Blackstocks <ankitkumarsoni656.iitkha@gmail.com>
2024-08-24 22:19:35 +0530

    gst fixed

--- pages/api/gstin.js
+++ pages/api/gstin.js
@@ -1,30 +1,25 @@
-// pages/api/gstin.js
 export default async function handler(req, res) {
   const { gstin } = req.query;
-  console.log(gstin);
 
   if (!gstin) {
     return res.status(400).json({ error: 'GSTIN is required' });
   }
 
   try {
-    const apiUrl = `https://apisetu.gov.in/gstn/v1/taxpayers/${gstin}`;
-    const options = {
+    const response = await fetch(`https://apisetu.gov.in/gstn/v1/taxpayers/${gstin}`, {
       method: 'GET',
       headers: {
         'accept': 'application/json',
         'X-APISETU-CLIENTID': process.env.X_APISETU_CLIENTID,
         'X-APISETU-APIKEY': process.env.X_APISETU_APIKEY,
-      },
-    };
-
-    const response = await fetch(apiUrl, options);
+      }
+    });
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch GSTIN data: ${response.statusText}`);
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
+
     const data = await response.json();
-    console.log(data)
     res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching GSTIN data:', error.message);
